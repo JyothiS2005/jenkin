54c078c45346e8978a814825fe74e9ec961847fe
JyothiS2005 <JyothiHudali05@gmail.com>
2025-04-22 14:33:23 +0530

    save the file

---
+++ Student.java
@@ -0,0 +1,16 @@
+package Demojenkins;
+
+public class Student {
+    public static void main(String[] args){
+        String name="Jyothi";
+        int id=072;
+        String Course="Devops";
+        String Branch="BCA";
+
+        System.out.println("Student Name:"+name);
+        System.out.println("Student id:"+id);
+        System.out.println("Student Course:"+Course);
+        System.out.println("Student Branch:"+Branch);
+    }
+}
+    
